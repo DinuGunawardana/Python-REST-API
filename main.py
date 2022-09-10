7806ffc94582893e8e21bd596f453ae7b8411738
cbabiomedical <90172658+cbabiomedical@users.noreply.github.com>
2022-09-10 17:50:33 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+api = Api(app)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
